ff3337016dd451731acea1894540f6be7b5d3d37
bo6eR-public <bo6eR.public@gmail.com>
2026-04-08 20:22:00 +0700

    Train model

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.pt")
 
 model.train(
-    data="My-First-Project-1/data.yaml",
+    data="My-First-Project-2/data.yaml",
     epochs=50,
     imgsz=640,
     batch=16,
