9481ecd13083d53d7365885c51f9469997005dda
bo6eR-public <bo6eR.public@gmail.com>
2026-04-08 19:25:59 +0700

    Change dataset version to 2 (add valid dataset)

--- load.py
+++ load.py
@@ -1,5 +1,5 @@
 from roboflow import Roboflow
 rf = Roboflow(api_key="beJvZ6246OssSRS6Weat")
 project = rf.workspace("s-workspace-mcr92").project("my-first-project-lj0to")
-version = project.version(1)
+version = project.version(2)
 dataset = version.download("yolov8")
